24a091ef5131043a164ba741d72350bbbf1c5903
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-28 19:13:30 +0000

    Fix issue with poorly formated prices. Linting.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ProductsController < ApplicationController
-
   def index
     @products = Product.order(id: :desc).all
   end
@@ -36,5 +35,4 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
 end

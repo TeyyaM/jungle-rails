cc13d72e6cd2114b9260970c6fc1f117db1d46ae
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-29 20:46:59 +0000

    Finalize login and add CSS

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Save the user id inside the browser cookie.
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      @message = 'Invalid email or password! Please try again.'
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    render :new
   end
 
 end

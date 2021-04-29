ffb31fecefb9fc100df2c1029f436856263317bd
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-29 20:23:01 +0000

    Add styling and bug fixes for registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      render :root
+      redirect_to '/'
     else
       @message = 'Invalid Entry! Please try again.'
-      # redirect_to '/signup'
       render :new
     end
   end   
--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,10 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  # resources :products, only: [:index, :show]
+
+  # These routes are for creating new users
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  post '/signup' => 'users#create'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

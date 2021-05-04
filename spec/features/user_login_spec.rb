ea4d3750c2e66aa05fbfc60fac3f91e74eaecf95
Tessa Azar MacLean <tessaamac@gmail.com>
2021-05-04 23:29:38 +0000

    Progress towards user login test

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor logs in and is redirected to home page", type: :feature do
+
+  # SETUP
+  before :each do
+    User.create!(
+      first_name: 'Egg',
+      last_name: 'Eggerson',
+      email: 'egg@egg.com',
+      password: 'egg',
+      password_confirmation: 'egg'
+    )
+  end
+
+  
+  scenario "They log in" do
+    # ACT
+    visit login_path
+    page.fill_in :session_email, with: 'egg@egg.com'
+    page.fill_in :session_password, with: 'egg'
+    # expect(page).to have_text 'Login/Register', count: 1
+    # click_button('Add', match: :first)
+    
+    # DEBUG
+    # sleep 1
+    save_screenshot 'test_four_user_login.png'
+
+    # VERIFY
+    # expect(page).to have_current_path(root_path)
+    # expect(page).to have_text 'Egg Eggerson', count: 1
+  end
+end
